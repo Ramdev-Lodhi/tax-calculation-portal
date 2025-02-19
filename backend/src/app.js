236fcf3f245bc3bbbea9c8023314c0f806a9308b
Ramdev Lodhi <ramdevlodhi9399@gmail.com>
2025-02-19 23:56:36 +0530

    fix: Update code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import router from "./routes/index.js"; 
+import router from "./routes/index.js";
 import database from "./config/db.js";
 
 dotenv.config();
@@ -10,8 +10,14 @@ const app = express();
 
 // Middleware
 // app.use(cors());
-app.use(cors({ origin: "*" }));
-
+// app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTION", "HEAD"],
+    origin: ["https://tax-calculation-portal-rl.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
